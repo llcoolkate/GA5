113b0158adc660efaf0335510b408787c65d286a
LLCoolKate <107154088+llcoolkate@users.noreply.github.com>
2022-07-13 16:45:07 -0500

    work around attempt

--- script.js
+++ script.js
@@ -16,5 +16,16 @@ window.addEventListener("load", function() {
        let planet = pickPlanet(listedPlanets)
        addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
    })
-   
+   // Validations 
+   let form = document.querySelector("form")
+   form.addEventListener("submit", function(event){
+      let pilot = document.querySelector("input[name = pilotName]")
+      let copilot = document.querySelector("input[name = copilotName]")
+      let cargoLevel = document.querySelector("input[name = cargoMass]")
+      let fuelLevel = document.querySelector("input[name = fuelLevel]")
+     
+   //let submitButton = document.getElementById("submitForm")
+   //submitButton.addEventListener("click", function(event) {
+     // event.preventDefault()
+     // let inputFields = Array.for,(document.querySe;ectorAll("input[type=text]"))
 });
